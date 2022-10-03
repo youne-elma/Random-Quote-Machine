283975f7b1ae3d851af04181fc38ce0e0de22c82
younelma <youne.elma@gmail.com>
2022-10-03 23:57:55 +0100

    feat[RQM]: css border designs

--- random-quote-machine/src/pages/main/index.js
+++ random-quote-machine/src/pages/main/index.js
@@ -2,18 +2,21 @@ import "./main.scss";
 
 function Main() {
   return (
-    <div id="quote-box" className="quoteBox">
-      <div className="cadreBox">
-        <h1 id="text" className="quoteText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </h1>
-        <div className="others-quote">
-          <button type="button">new quote</button>
-          <button type="button">
-            <a href="#link">tweetquote</a>
-          </button>
-          <p>quote author</p>
+    <div>
+      <span className="borderOne" />
+      <div id="quote-box" className="quoteBox">
+        <div className="cadreBox">
+          <h1 id="text" className="quoteText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </h1>
+          <div className="others-quote">
+            <button type="button">new quote</button>
+            <button type="button">
+              <a href="#link">tweetquote</a>
+            </button>
+            <p className="quote-author">quote author</p>
+          </div>
         </div>
       </div>
     </div>
