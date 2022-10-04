5027e516545f72ee3bebaeda8dd00bb923a6e24f
younelma <youne.elma@gmail.com>
2022-10-04 23:06:15 +0100

    feat[RQM]: css media and completing the design

--- random-quote-machine/src/pages/main/index.js
+++ random-quote-machine/src/pages/main/index.js
@@ -1,4 +1,6 @@
 import "./main.scss";
+import twitterLogo from "../../assets/twitterLogo.png";
+import quoteDesign from "../../assets/quotes.png";
 
 function Main() {
   return (
@@ -6,17 +8,23 @@ function Main() {
       <span className="borderOne" />
       <div id="quote-box" className="quoteBox">
         <div className="cadreBox">
+          <img alt="quotes design" className="quoteDesign1" src={quoteDesign} />
           <h1 id="text" className="quoteText">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </h1>
           <div className="others-quote">
-            <button type="button">new quote</button>
-            <button type="button">
-              <a href="#link">tweetquote</a>
+            <button type="button" className="nextQuote">
+              Next quote
             </button>
-            <p className="quote-author">quote author</p>
+            <button type="button" className="tweetQuote">
+              <a href="#link">
+                <img alt="twitter logo" src={twitterLogo} />
+              </a>
+            </button>
+            <p className="quote-author">Quote author</p>
           </div>
+          <img alt="quotes design" className="quoteDesign2" src={quoteDesign} />
         </div>
       </div>
     </div>
