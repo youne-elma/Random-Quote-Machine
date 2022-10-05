663705a3317091c0ac40139e6e7f7e1bbdb5cbf7
younelma <youne.elma@gmail.com>
2022-10-05 11:04:35 +0100

    feat[RQM]: adding the react-router-dom library

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Main from "./pages/main/index";
+import ErrorPage from "./pages/error/index";
 import "./App.scss";
 
 function App() {
   return (
-    <div id="wrapper" className="main">
-      <Main />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/random-quote-machine/"
+          exact
+          element={
+            <div id="wrapper" className="main">
+              <Main />
+            </div>
+          }
+        />
+        <Route path="/random-quote-machine/*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
