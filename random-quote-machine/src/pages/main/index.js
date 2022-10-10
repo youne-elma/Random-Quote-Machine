69fe8e24ea001eb7436ec7933e38594188e746c3
younelma <youne.elma@gmail.com>
2022-10-10 01:12:13 +0100

    feat[RQM]: link api in the main page

--- random-quote-machine/src/pages/main/index.js
+++ random-quote-machine/src/pages/main/index.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "./main.scss";
+import useGlobalContext from "../../context";
 import twitterLogo from "../../assets/twitterLogo.png";
 import quoteDesign from "../../assets/quotes.png";
 
 function Main() {
+  const quotes = useGlobalContext();
+  const [quote, setQuote] = useState({
+    quote:
+      "The two most important days in your life are the day you are born and the day you find out why.",
+    author: "Mark Twain",
+  });
+
+  function getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  }
+  function getQuote() {
+    const newQuote = getRandomQuote();
+    return newQuote;
+  }
+
   return (
     <div>
       <span className="borderOne" />
@@ -10,11 +27,14 @@ function Main() {
         <div className="cadreBox">
           <img alt="quotes design" className="quoteDesign1" src={quoteDesign} />
           <h1 id="text" className="quoteText">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            {quote.quote}
           </h1>
           <div className="others-quote">
-            <button type="button" className="nextQuote">
+            <button
+              type="button"
+              className="nextQuote"
+              onClick={() => setQuote(getQuote)}
+            >
               Next quote
             </button>
             <button type="button" className="tweetQuote">
@@ -22,7 +42,7 @@ function Main() {
                 <img alt="twitter logo" src={twitterLogo} />
               </a>
             </button>
-            <p className="quote-author">Quote author</p>
+            <p className="quote-author">{quote.author}</p>
           </div>
           <img alt="quotes design" className="quoteDesign2" src={quoteDesign} />
         </div>
